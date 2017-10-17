7945138c83f429a3de084c9f4d7818b89068a955
Matt Lee <matthewlee07@gmail.com>
2017-10-17 16:34:24 -0400

    nothing changed

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,10 @@
 import React from 'react';
 //need to make
 import Header from './header';
+import Restaurant from './restaurant';
 // import Container from './container'; 
-import {selectRestaurant} from '../actions';
-import {connect} from 'react-redux';
+import { selectRestaurant } from '../actions';
+import { connect } from 'react-redux';
 
 export class App extends React.Component {
   // eventDemo(e) { console.log('clicked me') }
@@ -14,15 +15,28 @@ export class App extends React.Component {
 
   render() {
 
-    
-    return (
-      <div>
-        <Header />
+    if (this.props.route === 'homepage') {
+      return (
+        <div>
+          <Header />
 
-        <div><button onClick={e => this.selectRestaurant(e)}>restaurant 1</button></div>
-      </div>
-    );
+          <div><button onClick={e => this.selectRestaurant(e)}>restaurant 1</button></div>
+        </div>
+      );
+    } else if (this.props.route === 'restaurant') {
+      return (
+        <div>
+          <Restaurant />
+
+          <div><button onClick={e => this.returnHomepage(e)}>go home</button></div>
+        </div>
+      );
+    }
   }
 };
 
-export default connect()(App);
+const mapStateToProps = state => ({
+  route: state.route
+});
+
+export default connect(mapStateToProps)(App);
