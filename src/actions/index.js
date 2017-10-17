f3ce5fb6aaf6de7092beb414701d9938bca4dd36
Matt Lee <matthewlee07@gmail.com>
2017-10-17 12:40:40 -0400

    something

---
+++ src/actions/index.js
@@ -0,0 +1,66 @@
+// import thunk from 'redux-thunk';
+// https://github.com/gaearon/redux-thunk
+export const ADD_USER = 'ADD_USER';
+export const addUser = (firstName, lastName, username, password) => ({
+  type: ADD_USER, 
+  firstName, lastName, username, password
+});
+
+export const SIGN_IN = 'SIGN_IN';
+export const signIn = (username, password) => ({
+  type: SIGN_IN,
+  username, password
+});
+
+export const SELECT_RESTAURANT = 'SELECT_RESTAURANT';
+export const selectRestaurant = () => ({
+  type: SELECT_RESTAURANT
+});
+
+export const RETURN_HOMEPAGE = 'RETURN_HOMPAGE';
+export const returnHomepage = () => ({
+  type: RETURN_HOMEPAGE
+});
+
+
+export const SCHEDULE_MEAL = 'SCHEDULE_MEAL';
+export const scheduleMeal = () => ({
+  type: SCHEDULE_MEAL
+});
+
+export const CONFIRM_RESERVATION = 'CONFIRM_RESERVATION';
+export const confirmReservation = () => ({
+
+});
+
+export const CREATE_USER = 'CREATE_USER';
+export const createUser = () => ({
+
+});
+
+export const FORGOT_PASSWORD = 'FORGOT_PASSWORD';
+export const forgotPassword = () => ({
+
+});
+
+export const EDITCANCEL = 'EDITCANCEL';
+export const editCancel = () => ({
+
+});
+
+export const INVITE = 'INVITE';
+export const invite = () => ({
+
+});
+
+export const fetchCheeses = () => (dispatch) => {
+  dispatch(fetchCheesesRequest());
+  return fetch(`${SERVER_URL}`)
+      .then(res => {
+          if (!res.ok) {
+              return dispatch(fetchCheesesError(res.statusText));
+          }
+          return res.json()
+      })
+      .then(cheeses => dispatch(fetchCheesesSuccess(cheeses)))
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux'; 
-import store from './store'; 
+const store = createStore(combinedReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
     <Provider store={store}>
