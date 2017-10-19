7bd8a868bf1ef275b443a471fd94c415a441e0b0
Anthony Mengel <anthonymengel1232@gmail.com>
2017-10-19 12:00:50 -0400

    added validators and local storage for auth prep

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { restaurantReducer } from './reducers';
+import  restaurantReducer  from './reducers/restaurant';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 const allReducers = combineReducers({
